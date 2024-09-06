0d72da6d82195a0f6d3be28ded5cdfc9a70ae49d
lpaixao- <leticiapwermelinger@gmail.com>
2024-09-06 18:55:44 -0300

    Rascunho

--- srcs/main/prompt_and_inputs.c
+++ srcs/main/prompt_and_inputs.c
@@ -6,7 +6,7 @@
 /*   By: lraggio <lraggio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 18:02:36 by lpaixao-          #+#    #+#             */
-/*   Updated: 2024/09/03 16:15:25 by lraggio          ###   ########.fr       */
+/*   Updated: 2024/09/06 17:23:55 by lpaixao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,28 +30,33 @@ char	*make_prompt(void)
 	free(temp);
 	return (prompt);
 }
-/*
+
 int	run_commands(t_command *command)
 {
-	t_node	*list;
+	t_node		*list;
+	t_tokens	*temp_words;
 	int		ret;
 
 	ret = NO_INFO;
 	list = command->l_input;
 	while (list)
 	{
-		if (is_builtin(list->value) == TRUE)
-			ret = run_builtin(command, list, 1); // a função está enviando fd 1 por padrão. deverá ser ajustado isso posteriormente para enviar um fd específico
-		else
-			printf("Não é builtin. Ainda estou criando as builtins\n");
-		if (ret == ERROR)
+		temp_words = list->token;
+		while (temp_words)
 		{
-//			printf("Validar erro\n");
-			return (ERROR);
+			if (is_builtin(temp_words) == TRUE)
+				ret = run_builtin(command, list, 1); // a função está enviando fd 1 por padrão. deverá ser ajustado isso posteriormente para enviar um fd específico
+			else
+				printf("Não é builtin. Ainda estou criando as builtins\n");
+			if (ret == ERROR)
+			{
+//				printf("Validar erro\n");
+				return (ERROR);
+			}
+			else if (ret == CLOSE)
+				return (CLOSE);
 		}
-		else if (ret == CLOSE)
-			return (CLOSE);
 		list = list->next;
 	}
 	return (ret);
-}*/
+}

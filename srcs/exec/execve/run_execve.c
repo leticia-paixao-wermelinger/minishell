ffaa57620484d9da0d0eee8becea4daf9ad8dd5a
Luara <luararaggio@hotmail.com>
2024-10-15 19:57:03 -0300

    ajuste de norma + ./temp/minishell

--- srcs/exec/execve/run_execve.c
+++ srcs/exec/execve/run_execve.c
@@ -6,39 +6,11 @@
 /*   By: lraggio <lraggio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 20:11:22 by lraggio           #+#    #+#             */
-/*   Updated: 2024/10/14 22:59:46 by lraggio          ###   ########.fr       */
+/*   Updated: 2024/10/15 19:49:51 by lraggio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
-
-/**
- * @brief check_executable_path - Checks for the executable path of a command.
- *
- * This function verifies if the command specified in the token can be
- * accessed and executed. If the command is not found or is not executable,
- * it attempts to retrieve the executable path using the PATH variable.
- * It returns the command path if accessible or NULL if an error occurs.
- *
- * @param cmd: A pointer to the command structure containing environment
- *             variables.
- * @param node: A pointer to the node containing the command token.
- * @param env_array: An array of environment variables used during execution.
- *
- * @return char*: The path of the executable or NULL if an error occurs.
- */
-
-char	*check_executable_path(t_command *cmd, t_node *node, char **env_array)
-{
-	if (access(node->token->word, (F_OK | X_OK)) != 0)
-	{
-		if (errno == EACCES)
-			return (print_errno(node), free_matrix(env_array), NULL);
-		return (get_executable_path(cmd, node));
-	}
-	else
-		return (node->token->word);
-}
 
 /**
  * @brief run_execve - Executes a command using execve.
@@ -48,12 +20,30 @@ char	*check_executable_path(t_command *cmd, t_node *node, char **env_array)
  * the input/output if needed, and calls execve to run the command.
  * The function cleans up allocated memory after execution.
  *
+ * - Checks for the executable path of a command.
+ *
+ * This function verifies if the command specified in the token can be
+ * accessed and executed. If the command is not found or is not executable,
+ * it attempts to retrieve the executable path using the PATH variable.
+ * It returns the command path if accessible or NULL if an error occurs.
+ *
  * @param command: A pointer to the command structure containing necessary
  *                 information for execution.
  * @param list: A pointer to the linked list of command nodes to be executed.
  *
  * @return void: This function does not return a value.
  */
+
+void	execve_fork(t_node *node, char *path, char **args, char **env_array)
+{
+	node->pid = fork();
+	if (node->pid == 0)
+	{
+		do_dup2(node);
+		close_node_fds(node);
+		execve(path, args, env_array);
+	}
+}
 
 void	run_execve(t_command *command, t_node *list)
 {
@@ -64,17 +54,18 @@ void	run_execve(t_command *command, t_node *list)
 
 	node = list;
 	env_array = envp_list_to_array(command->my_env);
-	path = check_executable_path(command, node, env_array);
-	if (!path)
-		return (free_matrix(env_array));
-	args = cmd_list_to_array(node);
-	node->pid = fork();
-	if (node->pid == 0)
+	if (access(node->token->word, (F_OK | X_OK)) != 0)
 	{
-		do_dup2(node);
-		close_node_fds(node);
-		execve(path, args, env_array);
+		if (errno == EACCES)
+			return (print_errno(node), free_matrix(env_array));
+		path = get_executable_path(command, node);
+		if (!path)
+			return (free_matrix(env_array));
 	}
+	else
+		path = node->token->word;
+	args = cmd_list_to_array(node);
+	execve_fork(node, path, args, env_array);
 	if (path != node->token->word)
 		free(path);
 	return (execve_clean(args, env_array));
